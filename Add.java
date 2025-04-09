31fed18452d87f32c753445ef7c5d6d03fceea59
mr-maktum-18 <maktumgudubhai@gmail.com>
2025-04-09 08:53:17 +0530

    sub add

--- Add.java
+++ Add.java
@@ -4,6 +4,8 @@ public class Add {
         int a=10;
         int b=20;
         int sum = a+b;
+        int sub = a-b;
         System.out.println("Sum is " + sum);
+        System.out.println("Sub is " + sub);
     }
 }
